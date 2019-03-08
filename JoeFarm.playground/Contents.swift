a5071491c89556f175a2394963b76d8c58062f03
Madaragog <cedric.dorez@gmail.com>
2019-03-08 02:08:42 +0100

    introduction to the loop "for"

--- JoeFarm.playground/Contents.swift
+++ JoeFarm.playground/Contents.swift
@@ -10,3 +10,15 @@ let result = price / dailyBenefit // 149
 
 let message = "Dear Joe, you'll need to save for \(result) to pay your \(price) euros trip"
 
+
+
+var money = 0.0
+
+for _ in 1...30 {
+    // Joe feeds the cows
+    money -= 4
+    // Joe sells milk
+    money += 30 * 0.50
+}
+
+print("\(money) €")// 330.0
