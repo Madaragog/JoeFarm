200a65d41fd499626752e0163b11b69cbfff28fa
Madaragog <cedric.dorez@gmail.com>
2019-03-10 23:44:22 +0100

    organising data in a dictionnary

--- JoeFarm.playground/Contents.swift
+++ JoeFarm.playground/Contents.swift
@@ -8,38 +8,42 @@ var money = 0.0
 var numberOfDay = 0
 
 // La grange de Joe : [lait, blé, laine]
-var barn = [0, 0, 0]
+var barn = ["milk": 0, "wheat": 0, "wool": 0]
 
+func calculateBarnSize() -> Int {
+    var barnSize = 0
+    for (_, count) in barn {
+        barnSize += count
+    }
+    
+    return barnSize
+}
 while money < price {
     // Joe nourrit les vaches tous les jours
     money -= 4
     
     // On calcule la taille de la grange
-    var barnSize = 0
-    for goods in barn {
-        barnSize += goods
-    }
     
-    if barnSize >= 500 {
+    if calculateBarnSize() > 500 {
         // On vends !
-        money += Double(barn[0]) * 0.50 // Le lait
-        money += Double(barn[1]) * 0.30 // Le blé
-        money += Double(barn[2]) * 1 // La laine
+        money += Double(barn["milk"]!) * 0.50 // Le lait
+        money += Double(barn["wheat"]!) * 0.30 // Le blé
+        money += Double(barn["wool"]!) * 1 // La laine
         
         // On vide la grange
-        barn = [0, 0, 0]
+        barn = ["milk": 0,"wheat": 0,"wool": 0]
     } else {
         // C'est une journée normale
         
         if numberOfDay % 30 == 1 {
             // Joe moissonne
-            barn[1] += 100
+            barn["wheat"]! += 100
         } else if numberOfDay % 30 == 10 || numberOfDay % 30 == 20 {
             // Joe tond les moutons
-            barn[2] += 30
+            barn["wool"]! += 30
         } else {
             // Joe trait les vaches
-            barn[0] += 30
+            barn["milk"]! += 30
         }
     }
     
@@ -48,3 +52,7 @@ while money < price {
 }
 
 print("Il aura fallu \(numberOfDay) jours à Joe pour économiser \(money) €")
+
+
+
+
